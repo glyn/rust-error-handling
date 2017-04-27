587d707af2fcca3a09fa27aabe59f798e46b5505
Glyn Normington <gnormington@pivotal.io>
2017-04-27 13:20:41 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+use std::path::PathBuf;
+use std::fs::File;
+use std::io::Read;
+
+pub fn read_file(path: PathBuf) -> Result<String, ParserError> {
+    match File::open(path) {
+        Err(err) => Err(ParserError::BadFile(err.to_string())),
+        Ok(file) => {
+            match size_from_file(file) {
+                Err(err) => Err(ParserError::BadSize(err)),
+                Ok(_) => Ok(String::from("File is non-empty"))
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ParserError {
+    BadFile(String),
+    BadSize(String),
+}
+
+fn size_from_file(mut file: File) -> Result<usize, String> {
+    let mut my_string = String::new();
+    match file.read_to_string(&mut my_string) {
+        Err(err) => Err(err.to_string()),
+        Ok(_) => {
+            match my_string.len() {
+                0 => Err(String::from("File is empty")),
+                l => Ok(l),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    #[test]
+    fn non_existent_file() {
+        let path = PathBuf::from("src/fixtures/no-such-file");
+
+        let result = super::read_file(path);
+        match result {
+            Err(err) => match err {
+                super::ParserError::BadFile(msg) => println!("{}", msg),
+                super::ParserError::BadSize(msg) => assert!(false, msg)
+            },
+            Ok(message) => assert!(false, message)
+        }
+    }
+
+    #[test]
+    fn empty_file() {
+        let path = PathBuf::from("src/fixtures/empty");
+
+        let result = super::read_file(path);
+        match result {
+            Err(err) => match err {
+                super::ParserError::BadFile(msg) => assert!(false, msg),
+                super::ParserError::BadSize(msg) => println!("{}", msg)
+            },
+            Ok(message) => assert!(false, message)
+        }
+    }
+
+    #[test]
+    fn non_empty_file() {
+        let path = PathBuf::from("src/fixtures/nonempty");
+
+        let result = super::read_file(path);
+        match result {
+            Err(err) => match err {
+                super::ParserError::BadFile(msg) => assert!(false, msg),
+                super::ParserError::BadSize(msg) => assert!(false, msg)
+            },
+            Ok(message) => println!("{}", message)
+        }
+    }
+}
